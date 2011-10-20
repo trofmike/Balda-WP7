0d04558744e3136b9fbeb34661a5ee97cd724a52
Mike Trof <trof.mike@gmail.com>
2011-10-21 03:04:17 +0400

    init project

---
+++ TextInputApp1/Alphabet.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace Balda
+{
+    public partial class Alphabet : PhoneApplicationPage
+    {
+        public Alphabet()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TextInputApp1/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace TextInputApp1
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        private object thatSender;
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            thatSender = sender;
+            Alphabet.Visibility = Visibility.Visible;
+        }
+
+        private void ApplicationBarIconButton_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            ((Button)thatSender).Content = ((Button)sender).Content;
+            Alphabet.Visibility = (Alphabet.Visibility == Visibility.Visible ? Visibility.Collapsed : Visibility.Visible);
+        }
+
+        protected override void OnBackKeyPress(System.ComponentModel.CancelEventArgs e)
+        {
+            base.OnBackKeyPress(e);
+            if (Alphabet.Visibility == Visibility.Visible)
+            {
+                Alphabet.Visibility = Visibility.Collapsed;
+                e.Cancel = true;
+            }
+        }
+    }
+}
